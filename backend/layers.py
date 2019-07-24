e0153ca0beb2487b6a20e85034632d8511614623
Jason Kuan (Astra) <jason9075@gmail.com>
2019-07-24 12:07:30 +0800

    change back to depthwise convolution

--- backend/layers.py
+++ backend/layers.py
@@ -10,8 +10,9 @@ else:
 D_TYPE = tf.float32
 CUDNN_ON_GPU = True
 BIAS_INIT = tf.constant_initializer(0.0)
+ONE_INIT = tf.constant_initializer(1.0)
 WEIGHT_INIT = tf.truncated_normal_initializer(stddev=0.02)
-REGULARIZER = tf.contrib.layers.l2_regularizer(2e-5)
+REGULARIZER = tf.contrib.layers.l2_regularizer(5e-6)
 
 
 def conv2d(x,
@@ -61,61 +62,8 @@ def depthwise_conv2d(x,
                 initializer=WEIGHT_INIT,
                 dtype=D_TYPE,
                 regularizer=REGULARIZER)
-            b = tf.get_variable(
-                name='b_dw',
-                shape=num_filter,
-                initializer=BIAS_INIT,
-                dtype=D_TYPE)
-            conv = tf.nn.depthwise_conv2d(
+            out = tf.nn.depthwise_conv2d(
                 x, w, stride, name='d_conv', padding='SAME')
-            out = tf.nn.bias_add(conv, b)
-            if bn:
-                out = tf.layers.batch_normalization(
-                    out, name='bn', training=is_train)
-            return act(out)
-
-
-def group_depthwise_conv2d(x,
-                           kernel,
-                           stride,
-                           num_groups=1,
-                           bn=True,
-                           act=tf.identity,
-                           name='conv_dw',
-                           is_train=True):
-    stride = [1, stride[0], stride[1], 1]
-    num_filter = x.shape[-1]
-    kernel_shape = [kernel[0], kernel[1], num_filter // num_groups, num_filter]
-    output_list = []
-
-    with tf.device(DEVICE):
-        with tf.variable_scope(name):
-            w = tf.get_variable(
-                name='w_dw',
-                shape=kernel_shape,
-                initializer=WEIGHT_INIT,
-                dtype=D_TYPE,
-                regularizer=REGULARIZER)
-            b = tf.get_variable(
-                name='b_dw',
-                shape=num_filter,
-                initializer=BIAS_INIT,
-                dtype=D_TYPE)
-
-            input_list = tf.split(x, num_groups, axis=-1)
-            filter_list = tf.split(w, num_groups, axis=-1)
-
-            for conv_idx, (input_tensor, filter_tensor) in enumerate(
-                    zip(input_list, filter_list)):
-                conv = tf.nn.depthwise_conv2d(
-                    input_tensor,
-                    filter_tensor,
-                    stride,
-                    name='d_conv',
-                    padding='SAME')
-                output_list.append(conv)
-            out = tf.concat(output_list, axis=-1)
-            out = tf.nn.bias_add(out, b)
             if bn:
                 out = tf.layers.batch_normalization(
                     out, name='bn', training=is_train)
@@ -160,56 +108,18 @@ def group_conv2d(x,
             return act(out)
 
 
-def group_conv2d_nobias(x,
-                        kernel,
-                        num_filter,
-                        stride,
-                        num_groups=1,
-                        bn=True,
-                        act=tf.identity,
-                        name='group_conv',
-                        padding='SAME',
-                        is_train=True):
-    pre_channel = int(x.get_shape()[-1])
-    shape = [kernel[0], kernel[1], pre_channel // num_groups, num_filter]
-
-    output_list = []
-
-    with tf.device(DEVICE):
-        with tf.variable_scope(name):
-            w = tf.get_variable(
-                name='w_conv',
-                shape=shape,
-                initializer=WEIGHT_INIT,
-                dtype=D_TYPE,
-                regularizer=REGULARIZER)
-            input_list = tf.split(x, num_groups, axis=-1)
-            filter_list = tf.split(w, num_groups, axis=-1)
-
-            for conv_idx, (input_tensor, filter_tensor) in enumerate(
-                    zip(input_list, filter_list)):
-                conv = tf.nn.convolution(
-                    input_tensor, filter_tensor, padding, stride, name=name)
-                output_list.append(conv)
-            out = tf.concat(output_list, axis=-1)
-            if bn:
-                out = tf.layers.batch_normalization(
-                    out, name='bn', training=is_train)
-            return act(out)
-
-
 def flatten(x, name='flatten'):
     all_dims_exc_first = np.prod([v.value for v in x.get_shape()[1:]])
     out = tf.reshape(x, [-1, all_dims_exc_first], name=name)
     return out
 
 
-def dense(x,
-          num_classes,
-          bn=True,
-          act=tf.identity,
-          name='dense',
-          is_train=True):
+def gdc_dense(x,
+              num_classes,
+              bn=True,
+              act=tf.identity,
+              name='dense',
+              is_train=True):
     n_in = int(x.get_shape()[-1])
     with tf.device(DEVICE):
         with tf.variable_scope(name):
@@ -226,5 +136,5 @@ def dense(x,
                 dtype=D_TYPE)
             out = tf.nn.bias_add(tf.matmul(x, w), b)
             if bn:
-                out = tf.layers.batch_normalization(out, training=is_train)
+                out = tf.layers.batch_normalization(out, training=is_train, gamma_initializer=ONE_INIT)
             return act(out)
--- backend/mobilenet_v1.py
+++ backend/mobilenet_v1.py
@@ -4,7 +4,7 @@ from backend.layers import *
 class MobileNetV1:
     def __init__(self, input_layer, is_train):
         bf = 32
-        with tf.variable_scope('mobilenet_v2'):
+        with tf.variable_scope('mobilenet_v1'):
             net = conv2d(
                 input_layer, (3, 3),
                 bf, (1, 1),
@@ -13,9 +13,8 @@ class MobileNetV1:
                 is_train=is_train)  # (112,112,32)
             # PS default stride is (2,2) but our training dataset (w,h) is 112, so stride is 1 here.
 
-            net = group_conv2d(
-                net, (3, 3), bf * 2, (1, 1),
-                num_groups=bf,
+            net = depthwise_conv2d(
+                net, (3, 3), (1, 1),
                 act=tf.nn.relu6,
                 name='conv_2_dw',
                 is_train=is_train)  # (112,112,32)
@@ -26,9 +25,8 @@ class MobileNetV1:
                 name='conv_2',
                 is_train=is_train)  # (112,112,32)
 
-            net = group_conv2d(
-                net, (3, 3), bf * 2, (2, 2),
-                num_groups=bf * 2,
+            net = depthwise_conv2d(
+                net, (3, 3), (2, 2),
                 act=tf.nn.relu6,
                 name='conv_3_dw',
                 is_train=is_train)  # (112,112,64)
@@ -39,9 +37,8 @@ class MobileNetV1:
                 name='conv_3',
                 is_train=is_train)  # (56,56,64)
 
-            net = group_conv2d(
-                net, (3, 3), bf * 4, (1, 1),
-                num_groups=bf * 4,
+            net = depthwise_conv2d(
+                net, (3, 3), (1, 1),
                 act=tf.nn.relu6,
                 name='conv_4_dw',
                 is_train=is_train)  # (56,56,128)
@@ -52,9 +49,8 @@ class MobileNetV1:
                 name='conv_4',
                 is_train=is_train)  # (56,56,128)
 
-            net = group_conv2d(
-                net, (3, 3), bf * 4, (2, 2),
-                num_groups=bf * 4,
+            net = depthwise_conv2d(
+                net, (3, 3), (2, 2),
                 act=tf.nn.relu6,
                 name='conv_5_dw',
                 is_train=is_train)  # (56,56,128)
@@ -65,9 +61,8 @@ class MobileNetV1:
                 name='conv_5',
                 is_train=is_train)  # (28,28,128)
 
-            net = group_conv2d(
-                net, (3, 3), bf * 8, (1, 1),
-                num_groups=bf * 8,
+            net = depthwise_conv2d(
+                net, (3, 3), (1, 1),
                 act=tf.nn.relu6,
                 name='conv_6_dw',
                 is_train=is_train)  # (28,28,256)
@@ -78,9 +73,8 @@ class MobileNetV1:
                 name='conv_6',
                 is_train=is_train)  # (28,28,256)
 
-            net = group_conv2d(
-                net, (3, 3), bf * 8, (2, 2),
-                num_groups=bf * 8,
+            net = depthwise_conv2d(
+                net, (3, 3), (2, 2),
                 act=tf.nn.relu6,
                 name='conv_7_dw',
                 is_train=is_train)  # (28,28,256)
@@ -91,9 +85,8 @@ class MobileNetV1:
                 name='conv_7',
                 is_train=is_train)  # (14,14,256)
 
-            net = group_conv2d(
-                net, (3, 3), bf * 16, (1, 1),
-                num_groups=bf * 16,
+            net = depthwise_conv2d(
+                net, (3, 3), (1, 1),
                 act=tf.nn.relu6,
                 name='conv_8_1_dw',
                 is_train=is_train)  # (14,14,512)
@@ -103,9 +96,8 @@ class MobileNetV1:
                 act=tf.nn.relu6,
                 name='conv_8_1',
                 is_train=is_train)
-            net = group_conv2d(
-                net, (3, 3), bf * 16, (1, 1),
-                num_groups=bf * 16,
+            net = depthwise_conv2d(
+                net, (3, 3), (1, 1),
                 act=tf.nn.relu6,
                 name='conv_8_2_dw',
                 is_train=is_train)
@@ -115,9 +107,8 @@ class MobileNetV1:
                 act=tf.nn.relu6,
                 name='conv_8_2',
                 is_train=is_train)
-            net = group_conv2d(
-                net, (3, 3), bf * 16, (1, 1),
-                num_groups=bf * 16,
+            net = depthwise_conv2d(
+                net, (3, 3), (1, 1),
                 act=tf.nn.relu6,
                 name='conv_8_3_dw',
                 is_train=is_train)
@@ -127,9 +118,8 @@ class MobileNetV1:
                 act=tf.nn.relu6,
                 name='conv_8_3',
                 is_train=is_train)
-            net = group_conv2d(
-                net, (3, 3), bf * 16, (1, 1),
-                num_groups=bf * 16,
+            net = depthwise_conv2d(
+                net, (3, 3), (1, 1),
                 act=tf.nn.relu6,
                 name='conv_8_4_dw',
                 is_train=is_train)
@@ -139,9 +129,8 @@ class MobileNetV1:
                 act=tf.nn.relu6,
                 name='conv_8_4',
                 is_train=is_train)
-            net = group_conv2d(
-                net, (3, 3), bf * 16, (1, 1),
-                num_groups=bf * 16,
+            net = depthwise_conv2d(
+                net, (3, 3), (1, 1),
                 act=tf.nn.relu6,
                 name='conv_8_5_dw',
                 is_train=is_train)
@@ -152,9 +141,8 @@ class MobileNetV1:
                 name='conv_8_5',
                 is_train=is_train)
 
-            net = group_conv2d(
-                net, (3, 3), bf * 16, (2, 2),
-                num_groups=bf * 16,
+            net = depthwise_conv2d(
+                net, (3, 3), (2, 2),
                 act=tf.nn.relu6,
                 name='conv_9_dw',
                 is_train=is_train)  # (14,14,512)
@@ -165,9 +153,8 @@ class MobileNetV1:
                 name='conv_9',
                 is_train=is_train)  # (7,7,1024)
 
-            net = group_conv2d(
-                net, (3, 3), bf * 32, (1, 1),
-                num_groups=bf * 32,
+            net = depthwise_conv2d(
+                net, (3, 3), (1, 1),
                 act=tf.nn.relu6,
                 name='conv_10_dw',
                 is_train=is_train)  # (7,7,1024)
@@ -179,11 +166,11 @@ class MobileNetV1:
                 is_train=is_train)  # (7,7,1024)
 
         with tf.variable_scope('gdc'):
-            net = group_conv2d_nobias(
+            net = group_conv2d(
                 net, (7, 7),
                 512, (1, 1),
                 num_groups=512,
                 name='conv',
                 is_train=is_train)  # (1,1,512)
-            self.embedding = dense(
+            self.embedding = gdc_dense(
                 flatten(net), 128, name='embedding', is_train=is_train)
