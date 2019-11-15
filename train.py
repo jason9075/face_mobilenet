215d9de7029beb1c1d53d2f19059fca9bd90534d
Jason Kuan (Astra) <jason9075@gmail.com>
2019-11-15 17:52:21 +0800

    extract lfw test out

--- train.py
+++ train.py
@@ -3,12 +3,10 @@ import glob
 import logging
 import logging.handlers as handlers
 import os
-import pickle
 import time
 from datetime import datetime
 
 import cv2
-# import mxnet as mx
 import numpy as np
 import tensorflow as tf
 from sklearn import preprocessing
@@ -315,32 +313,9 @@ def show_info(acc_val, step, i, images_train, inference_loss_val, input_layer,
     log('(%d vs %d)distance: %.2f' % (labels_train[0], labels_train[1], dist))
 
 
-def load_bin(bin_path):
-    bins, issame_list = pickle.load(open(bin_path, 'rb'), encoding='bytes')
-    data_list = []
-    for _ in [0, 1]:
-        data = np.empty((len(issame_list) * 2, INPUT_SIZE[0], INPUT_SIZE[1], 3))
-        data_list.append(data)
-    for i in range(len(issame_list) * 2):
-        _bin = bins[i]
-        img = mx.image.imdecode(_bin).asnumpy()
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        for flip in [0, 1]:
-            if flip == 1:
-                img = np.fliplr(img)
-            data_list[flip][i, ...] = img
-        i += 1
-        if i % 1000 == 0:
-            print('loading bin', i)
-    print(data_list[0].shape)
-    return data_list, issame_list
-
-
 def test():
     verification_path = os.path.join('tfrecord', 'verification.tfrecord')
     ver_dataset = utils.get_ver_data(verification_path)
-
-    # lfw_set = load_bin('tfrecord/lfw.bin')
 
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph(
@@ -368,15 +343,6 @@ def test():
             input_placeholder=input_tensor)
 
         print('astra acc: %.2f, thr: %.2f' % (val_acc, val_thr))
-
-        # val_acc, val_thr = utils.lfw_test(
-        #     data_set=lfw_set,
-        #     sess=sess,
-        #     embedding_tensor=embedding_tensor,
-        #     feed_dict=feed_dict_test,
-        #     input_placeholder=input_tensor)
-        #
-        # print('lfw acc: %.2f, thr: %.2f' % (val_acc, val_thr))
 
         # feed_dict = {input_tensor: np.expand_dims(image1, 0), trainable: False}
         # vector1 = sess.run(embedding_tensor, feed_dict=feed_dict)
