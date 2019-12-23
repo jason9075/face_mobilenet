79e7f9218b0289e295d412a781f5f4e97778209d
Jason Kuan (Astra) <jason9075@gmail.com>
2019-12-23 20:40:56 +0800

    add train_and_evaluate on script

--- train_by_estimator.py
+++ train_by_estimator.py
@@ -18,11 +18,16 @@ def main():
                                     save_summary_steps=params.save_summary_steps)
 
     estimator = tf.estimator.Estimator(model_fn, params=params, config=config)
-    estimator.train(lambda: train_input_fn('train.tfrecord', params))
 
-    res = estimator.evaluate(lambda: test_input_fn('train.tfrecord', params))
-    for key in res:
-        print("{}: {}".format(key, res[key]))
+    train_spec = tf.estimator.TrainSpec(input_fn=lambda: train_input_fn('train_1036.tfrecord', params), max_steps=1000)
+    eval_spec = tf.estimator.EvalSpec(input_fn=lambda: test_input_fn('valid_1036.tfrecord', params), steps=None,
+                                      throttle_secs=60, start_delay_secs=0)
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+
+    # estimator.train(lambda: train_input_fn('train.tfrecord', params))
+    # res = estimator.evaluate(lambda: test_input_fn('train.tfrecord', params))
+    # for key in res:
+    #     print("{}: {}".format(key, res[key]))
 
 
 if __name__ == '__main__':
